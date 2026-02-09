996a20335b64dfc51bc10e5aebd93cb22fc05195
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2026-02-09 18:40:02 -0500

    migrate atomic-fedora

--- Containerfile
+++ Containerfile
@@ -1,18 +1,9 @@
-# Allow build scripts to be referenced without being copied into the final image
 FROM scratch AS ctx
 COPY build_files /
+COPY post-install-checker /post-install-checker
+COPY ujust /ujust/
 
-# Base Image
-FROM ghcr.io/ublue-os/bazzite:stable
-
-## Other possible base images include:
-# FROM ghcr.io/ublue-os/bazzite:latest
-# FROM ghcr.io/ublue-os/bluefin-nvidia:stable
-# 
-# ... and so on, here are more base images
-# Universal Blue Images: https://github.com/orgs/ublue-os/packages
-# Fedora base image: quay.io/fedora/fedora-bootc:41
-# CentOS base images: quay.io/centos-bootc/centos-bootc:stream10
+FROM ghcr.io/ublue-os/bluefin-dx-nvidia-open:stable
 
 ### [IM]MUTABLE /opt
 ## Some bootable images, like Fedora, have /opt symlinked to /var/opt, in order to
@@ -22,19 +13,21 @@ FROM ghcr.io/ublue-os/bazzite:stable
 ##
 ## Uncomment the following line if one desires to make /opt immutable and be able to be used
 ## by the package manager.
-
 # RUN rm /opt && mkdir /opt
-
-### MODIFICATIONS
-## make modifications desired in your image and install packages by modifying the build.sh script
-## the following RUN directive does all the things required to run "build.sh" as recommended.
 
 RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/cache \
     --mount=type=cache,dst=/var/log \
     --mount=type=tmpfs,dst=/tmp \
     /ctx/build.sh
-    
+
+### Custom ujust tasks
+COPY --from=ctx /ujust/60-custom.just /usr/share/ublue-os/just/60-custom.just
+
+### Post-install checker (clones repo on first login for ujust do-post-install)
+COPY --from=ctx /post-install-checker/post-install-checker.service /usr/lib/systemd/user/post-install-checker.service
+RUN systemctl --global enable post-install-checker.service
+
 ### LINTING
 ## Verify final image and contents are correct.
 RUN bootc container lint
--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,227 @@
 
 set -ouex pipefail
 
-### Install packages
+dnf5 clean all
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+######################
+## GENERAL PACKAGES ##
+######################
+echo "::group:: Install general packages"
+GENERAL_PACKAGES=(
+  "vlc"
+  "konsole"
+  "okular"
+  "dbus-devel"
+  "dnf-command(copr)"
+)
+dnf5 install -y "${GENERAL_PACKAGES[@]}"
+echo "::endgroup::"
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+###############
+## DEV TOOLS ##
+###############
+echo "::group:: Install development tools"
+dnf5 install -y @development-tools
+DEV_PACKAGES=(
+  "zlib-devel"
+  "bzip2-devel"
+  "readline-devel"
+  "sqlite"
+  "sqlite-devel"
+  "openssl-devel"
+  "xz-devel"
+  "libffi-devel"
+  "findutils"
+  "tk-devel"
+  "libzstd-devel"
+  "autoconf"
+  "gcc"
+  "rust"
+  "patch"
+  "libyaml-devel"
+  "gdbm-devel"
+  "ncurses-devel"
+  "perl-FindBin"
+  "git-delta"
+  "tmux"
+)
+echo "Installing dev packages ${#DEV_PACKAGES[@]} ..."
+dnf5 install -y "${DEV_PACKAGES[@]}"
+echo "::endgroup::"
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+###############
+## CLI TOOLS ##
+###############
+echo "::group:: Install CLI tools"
+CLI_PACKAGES=(
+  "atuin"
+  "btop"
+  "bat"
+  "chezmoi"
+  "ripgrep"
+  "zoxide"
+  "fd-find"
+  "trash-cli"
+  "tree-sitter-cli"
+)
+echo "Installing cli packages ${#CLI_PACKAGES[@]} ..."
+dnf5 install -y "${CLI_PACKAGES[@]}"
+echo "::endgroup::"
+
+##########
+## YAZI ##
+##########
+echo "::group:: Install yazi"
+dnf5 copr enable -y lihaohong/yazi
+dnf5 install -y yazi
+echo "::endgroup::"
+
+#############
+## WEZTERM ##
+#############
+echo "::group:: Install wezterm"
+dnf5 copr enable -y wezfurlong/wezterm-nightly
+dnf5 install -y wezterm
+echo "::endgroup::"
+
+############
+## VSCODE ##
+############
+echo "::group:: Install vscode"
+rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
+  echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" |
+  tee /etc/yum.repos.d/vscode.repo >/dev/null
+dnf5 install -y code
+echo "::endgroup::"
+
+#################
+## ANTIGRAVITY ##
+#################
+echo "::group:: Install antigravity"
+tee /etc/yum.repos.d/antigravity.repo <<EOF
+[antigravity-rpm]
+name=Antigravity RPM Repository
+baseurl=https://us-central1-yum.pkg.dev/projects/antigravity-auto-updater-dev/antigravity-rpm
+enabled=1
+gpgcheck=0
+EOF
+dnf5 makecache
+dnf5 install -y antigravity
+echo "::endgroup::"
+
+###############
+## COSMIC DE ##
+###############
+echo "::group:: Install COSMIC Desktop"
+dnf5 install -y @cosmic-desktop-environment
+echo "COSMIC desktop installed successfully"
+echo "::endgroup::"
+
+echo "::group:: Configure Display Manager"
+systemctl disable cosmic-greeter || true
+systemctl enable gdm
+echo "Display manager configured"
+echo "::endgroup::"
+
+echo "::group:: Install Additional Utilities"
+dnf5 install -y \
+  xdg-desktop-portal-cosmic
+echo "Additional utilities installed"
+echo "::endgroup::"
+
+#############
+## FIREFOX ##
+#############
+echo "::group:: Install firefox"
+dnf5 config-manager setopt fedora-cisco-openh264.enabled=1
+dnf5 install -y firefox --allowerasing
+echo "::endgroup::"
+
+###############
+## 1PASSWORD ##
+###############
+echo "::group:: Install 1password"
+rpm --import https://downloads.1password.com/linux/keys/1password.asc
+dnf5 config-manager addrepo \
+  --id=1password \
+  --set=name="1Password Stable Channel" \
+  --set=baseurl="https://downloads.1password.com/linux/rpm/stable/\$basearch" \
+  --set=enabled=1 \
+  --set=gpgcheck=1 \
+  --set=repo_gpgcheck=1 \
+  --set=gpgkey="https://downloads.1password.com/linux/keys/1password.asc"
+dnf5 install -y 1password
+echo "::endgroup::"
+
+###################
+## GOOGLE CHROME ##
+###################
+echo "::group:: Install google chrome"
+dnf5 install -y fedora-workstation-repositories
+dnf5 config-manager setopt google-chrome.enabled=1
+dnf5 install -y google-chrome-stable
+echo "::endgroup::"
+
+###########
+## SLACK ##
+###########
+echo "::group:: Install slack"
+rpm --import https://packagecloud.io/slacktechnologies/slack/gpgkey
+cat <<'EOF' | tee /etc/yum.repos.d/slack.repo
+[slack]
+name=Slack
+baseurl=https://packagecloud.io/slacktechnologies/slack/fedora/21/$basearch
+enabled=1
+gpgcheck=0
+repo_gpgcheck=0
+gpgkey=https://packagecloud.io/slacktechnologies/slack/gpgkey
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+metadata_expire=300
+EOF
+dnf5 -y --refresh makecache
+dnf5 -y install slack
+echo "::endgroup::"
+
+###########
+## TEAMS ##
+###########
+echo "::group:: Install teams-for-linux"
+curl -1sLf -o /tmp/teams-for-linux.asc https://repo.teamsforlinux.de/teams-for-linux.asc
+rpm --import /tmp/teams-for-linux.asc
+curl -1sLf -o /etc/yum.repos.d/teams-for-linux.repo https://repo.teamsforlinux.de/rpm/teams-for-linux.repo
+dnf5 -y install teams-for-linux
+echo "::endgroup::"
+
+#############
+## SUBLIME ##
+#############
+echo "::group:: Install sublime-text"
+rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+dnf5 config-manager addrepo --from-repofile=https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+dnf5 makecache
+dnf5 install -y sublime-text
+echo "::endgroup::"
+
+###############
+## RPMFUSION ##
+###############
+echo "::group:: Install rpmfusion"
+dnf5 install \
+  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
+  -y
+echo "::endgroup::"
+
+###########
+## STEAM ##
+###########
+echo "::group:: Install steam"
+dnf5 install -y steam
+echo "::endgroup::"
 
 #### Example for enabling a System Unit File
-
 systemctl enable podman.socket
+
+dnf5 clean all
+echo "🚀 Installation complete!"
