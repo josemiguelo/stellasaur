5106c7a179d1199717503eb74ab6df7bea8720b1
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2026-02-27 11:47:41 -0500

    stop using cosmic desktop

--- build_files/build.sh
+++ build_files/build.sh
@@ -84,22 +84,22 @@ echo "::endgroup::"
 ###############
 ## COSMIC DE ##
 ###############
-echo "::group:: Install COSMIC Desktop"
-dnf5 install -y @cosmic-desktop-environment
-echo "COSMIC desktop installed successfully"
-echo "::endgroup::"
-
-echo "::group:: Configure Display Manager"
-systemctl disable cosmic-greeter || true
-systemctl enable gdm
-echo "Display manager configured"
-echo "::endgroup::"
-
-echo "::group:: Install Additional Utilities"
-dnf5 install -y \
-  xdg-desktop-portal-cosmic
-echo "Additional utilities installed"
-echo "::endgroup::"
+# echo "::group:: Install COSMIC Desktop"
+# dnf5 install -y @cosmic-desktop-environment
+# echo "COSMIC desktop installed successfully"
+# echo "::endgroup::"
+#
+# echo "::group:: Configure Display Manager"
+# systemctl disable cosmic-greeter || true
+# systemctl enable gdm
+# echo "Display manager configured"
+# echo "::endgroup::"
+#
+# echo "::group:: Install Additional Utilities"
+# dnf5 install -y \
+#   xdg-desktop-portal-cosmic
+# echo "Additional utilities installed"
+# echo "::endgroup::"
 
 #############
 ## FIREFOX ##
